e2f69a252d9bd0585aad593e573494a2bf452783
Marwan Fahad <mamemo1270@gmail.com>
2025-07-25 15:23:47 +0300

    secend update

--- algorithm1/algorithm1.py
+++ algorithm1/algorithm1.py
@@ -1,26 +1,29 @@
 def knapsack(weights, values, capacity, index):
-    # Base case: if no items left or the capacity is full
+    # This is the base case , if we go thrgouth all pakages index = -1 end of list
+    # or capacity is full  capacity =  0
     if index == -1 or capacity == 0:
         return 0
 
-    # If weight of the current package is more than the remaining capacity, exclude it
+    # If weight of current package is  more then capacity , no need to complate skip to next package ,
+    # like if you have a truck and you want to load it in other smaller track , it can not be and no need to look solution
     if weights[index ] > capacity:
         return knapsack(weights, values, capacity, index - 1)
 
-    # Option 1: Exclude the current package
+    # case 1 - exclude
     exclude = knapsack(weights, values, capacity, index - 1)
 
-    # Option 2: Include the current package (if it fits)
+    # case 2 - include
     include = values[index ] + knapsack(weights, values, capacity - weights[index ], index - 1)
 
-    # Return the maximum of both options (include or exclude)
+    # Return the max of include and exclude , more : it will be like tree until it reach base case
+    # then cacluate max as from bottom to up the tree
     return max(include, exclude)
 
 # input example
-weights = [10, 20, 30]
-values = [60, 100, 120]
-capacity = 50
-indexes = len(weights) - 1
+weights = [10, 20, 30] # the weights for each pakages,
+values = [60, 100, 120] # the values for each pakages,
+capacity = 50  # maxaimum capacity
+indexes = len(weights) - 1            # indexes start for top 2 to last one 0  in this example
 
 # Call the knapsack function
 max_value = knapsack(weights, values, capacity, indexes)
